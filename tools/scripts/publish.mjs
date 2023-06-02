95c30bdc87b6f119bd79591cd96877bd13798383
Dominic Egginton <dominic.egginton@gmail.com>
2023-06-02 16:25:04 +0100

    chore(workspace): create publish script

---
+++ tools/scripts/publish.mjs
@@ -0,0 +1,47 @@
+import { execSync } from 'child_process';
+import { readFileSync, writeFileSync } from 'fs';
+import chalk from 'chalk';
+import { readCachedProjectGraph } from '@nx/devkit';
+
+function invariant(condition, message) {
+  if (!condition) {
+    console.error(chalk.bold.red(message));
+    process.exit(1);
+  }
+}
+
+const [, , name, version, tag = 'next'] = process.argv;
+
+const validVersion = /^\d+\.\d+\.\d+(-\w+\.\d+)?/;
+invariant(
+  version && validVersion.test(version),
+  `No version provided or version did not match Semantic Versioning, expected: #.#.#-tag.# or #.#.#, got ${version}.`
+);
+
+const graph = readCachedProjectGraph();
+const project = graph.nodes[name];
+
+invariant(
+  project,
+  `Could not find project "${name}" in the workspace. Is the project.json configured correctly?`
+);
+
+const outputPath = project.data?.targets?.build?.options?.outputPath;
+invariant(
+  outputPath,
+  `Could not find "build.options.outputPath" of project "${name}". Is project.json configured  correctly?`
+);
+
+process.chdir(outputPath);
+
+try {
+  const json = JSON.parse(readFileSync(`package.json`).toString());
+  json.version = version;
+  writeFileSync(`package.json`, JSON.stringify(json, null, 2));
+} catch (e) {
+  console.error(
+    chalk.bold.red(`Error reading package.json file from library build output.`)
+  );
+}
+
+execSync(`npm publish --@rxtp:registry=https://npm.pkg.github.com/rxtp --access public --tag ${tag}`);
