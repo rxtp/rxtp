2a6d5f4f79ee76432996ea1762c08dbd20f0742d
Dominic Egginton <dominic.egginton@gmail.com>
2024-02-14 09:58:38 +0000

    chore(workspace): use napalm nix module to build

--- flake.nix
+++ flake.nix
@@ -1,84 +1,33 @@
 {
   inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
   inputs.systems.url = "github:nix-systems/default";
+  inputs.napalm.url = "github:nix-community/napalm";
 
   outputs = {
     self,
     nixpkgs,
     flake-utils,
     systems,
+    napalm,
   }:
     flake-utils.lib.eachSystem (import systems)
     (system: let
       pkgs = import nixpkgs {inherit system;};
-      node = pkgs.nodejs_20;
-      src = ./.;
-      package = builtins.fromJSON (builtins.readFile (src + "/package.json"));
-      package-lock = builtins.fromJSON (builtins.readFile (src + "/package-lock.json"));
-
-      deps =
-        pkgs.lib.attrValues (removeAttrs package-lock.packages [""]);
-
-      tarballs = map (dep:
-        pkgs.fetchurl {
-          url = dep.resolved;
-          hash = dep.integrity;
-        })
-      deps;
-
-      tarballsFile = pkgs.writeTextFile {
-        name = "tarballs";
-        text = builtins.concatStringsSep "\n" tarballs;
-      };
-
-      cacache =
-        pkgs.runCommand "${package.name}-cacache" {
-          passAsFile = ["tarballs"];
-          tarballs = pkgs.lib.concatLines tarballs;
-        }
-        ''
-          while read -r tarball; do
-            echo "caching $tarball" >&2
-            ${node}/bin/npm cache add --cache . "$tarball"
-          done < "$tarballsPath"
-          ${pkgs.coreutils}/bin/cp -r _cacache $out
-        '';
-
-      node_modules = pkgs.stdenv.mkDerivation {
-        name = "${package.name}-node_modules";
-        src = pkgs.runCommand "${package.name}-node-modules-src" {} ''
-          mkdir -p $out/.npm
-          ln -s ${cacache} $out/.npm/_cacache
-          ln -s ${src + "/package-lock.json"} $out/package-lock.json
-        '';
-        buildInputs = [node];
-        outputs = ["out"];
-        buildPhase = ''
-          export HOME=$PWD
-          npm ci --ignore-scripts
-          mv node_modules $out
-          chmod -R +w $out
-        '';
-      };
+      nodejs = pkgs.nodejs_20;
     in {
-      packages.default = pkgs.stdenv.mkDerivation {
-        inherit (package-lock) name version;
-        inherit src;
-        buildInputs = [node node_modules];
-        buildPhase = ''
-          ln -s ${node_modules.out} node_modules
-          export PATH=$PATH:$PWD/node_modules/.bin
-          npx nx run-many --target=build
-        '';
-
+      packages.default = napalm.legacyPackages.${system}.buildPackage ./. {
+        inherit nodejs;
+        npmCommands = [
+          "npm ci --ignore-scripts"
+          "npx nx run-many --target build"
+        ];
         installPhase = ''
-          mkdir -p $out
-          cp -r dist $out
+          ${pkgs.coreutils}/bin/mv dist/* $out
         '';
       };
 
       devShells.default = pkgs.mkShell rec {
-        buildInputs = with pkgs; [node];
+        buildInputs = [nodejs];
       };
     });
 }
