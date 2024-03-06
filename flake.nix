766a319e04e9af929cee53553432fafbb49c5b51
Dominic Egginton <dominic.egginton@gmail.com>
2024-03-06 11:58:36 +0000

    feat(workspace): update flake to use noxide for building

--- flake.nix
+++ flake.nix
@@ -1,33 +1,61 @@
 {
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/master";
-  inputs.systems.url = "github:nix-systems/default";
-  inputs.napalm.url = "github:nix-community/napalm";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixpkgs-unstable";
+  inputs.noxide.url = "github:dominicegginton/noxide";
 
   outputs = {
     self,
     nixpkgs,
-    flake-utils,
-    systems,
-    napalm,
-  }:
-    flake-utils.lib.eachSystem (import systems)
-    (system: let
-      pkgs = import nixpkgs {inherit system;};
-      nodejs = pkgs.nodejs_20;
-    in {
-      packages.default = napalm.legacyPackages.${system}.buildPackage ./. {
-        inherit nodejs;
-        npmCommands = [
-          "npm ci --ignore-scripts"
-          "npx nx run-many --target build"
-        ];
-        installPhase = ''
-          ${pkgs.coreutils}/bin/mv dist/* $out
-        '';
-      };
+    noxide,
+  }: let
+    supportedSystems = ["x86_64-linux" "i686-linux" "x86_64-darwin"];
 
-      devShells.default = pkgs.mkShell rec {
-        buildInputs = [nodejs];
+    forAllSystems = f:
+      nixpkgs.lib.genAttrs supportedSystems (system: f system);
+
+    nixpkgsFor = forAllSystems (system:
+      import nixpkgs {
+        inherit system;
+        overlays = [
+          self.overlays.default
+          noxide.overlays.default
+        ];
+      });
+
+    node = forAllSystems (system: nixpkgsFor.${system}.nodejs_20);
+  in {
+    formatter = forAllSystems (
+      system:
+        nixpkgsFor.${system}.alejandra
+    );
+
+    overlays = {
+      default = final: prev: {
+        rxtp = noxide.legacyPackages.${final.system}.buildPackage ./. {
+          nodejs = node.${final.system};
+
+          npmCommands = [
+            # ignore scripts due to @parcel/watcher postinstall script error
+            "npm install --no-audit --no-fund --ignore-scripts"
+            "npx nx run-many --target build"
+          ];
+
+          installPhase = ''
+            mkdir -p $out
+            mv dist/packages/* $out
+          '';
+        };
+      };
+    };
+
+    packages = forAllSystems (system: {
+      inherit (nixpkgsFor.${system}) rxtp;
+      default = nixpkgsFor.${system}.rxtp;
+    });
+
+    devShells = forAllSystems (system: {
+      default = nixpkgsFor.${system}.mkShell {
+        buildInputs = with nixpkgsFor.${system}; [node.${system}];
       };
     });
+  };
 }
